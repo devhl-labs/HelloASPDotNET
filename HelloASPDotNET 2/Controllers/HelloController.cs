32069abc1c83fa56d70aa4474f117159ab2c3f03
devhl-labs <shawnkanyer@gmail.com>
2020-06-22 13:02:21 -0400

    completed exercise

---
+++ HelloASPDotNET 2/Controllers/HelloController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET_2.Controllers
+{
+    public class HelloController : Controller
+    {
+        public static string CreateMessage(string language, string name)
+        {
+            string result = $"Hello {name}.";
+
+            if (language == "es")
+                result = $"Holla {name}.";
+
+            if (language == "fr")
+                result = $"Bonjour {name}.";
+
+            if (language == "ar")
+                result = $".{name} مرحبا";
+
+            if (language == "ja")
+                result = $"こんにちは {name}.";
+
+            return result;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form action='hello/welcome' method='post'>" +
+                            "<input required name='name' value='Your Name'></input>" +
+                            "<select required name='language'>" +
+                                "<option>--Select Language--</option>" +
+                                "<option value='en'>English</option>" +
+                                "<option value='es'>Spanish</option>" +
+                                "<option value='fr'>French</option>" +
+                                "<option value='ar'>Arabic</option>" +
+                                "<option value='ja'>Japanese</option>" +
+                            "</select>" +
+                            "<input type='submit' value='Submit'>" +
+                          "</form>";
+
+            return Content(html, "text/html");
+        }
+
+        //[HttpPost]
+        //[HttpGet]
+        [Route("hello/welcome/{name?}")]
+        public IActionResult Welcome(string language = "en", string name = ", World")
+        {
+            return Content(CreateMessage(language, name), "text/html");
+        }
+    }
+}
---
+++ HelloASPDotNET 2/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HelloASPDotNET_2.Models;
+
+namespace HelloASPDotNET_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: /Home/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+            //string html = "<form action='post'>" +
+            //                "<input value='Your Name'></input>" +
+            //                "<select>" +
+            //                    "<option>--Select Language--</option>" +
+            //                    "<option value='1'>English</option>" +
+            //                "</select>" +
+            //                "<input type='submit' value='Submit'>" +
+            //              "</form>";
+
+            //return Content(html, "text/html");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
